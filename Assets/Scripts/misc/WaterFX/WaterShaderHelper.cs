59c617609abc6d2b50e15919fc26a971e9e4bb6a
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-03-26 09:03:15 +0200

    WaterShaderHelper to C#

---
+++ Assets/Scripts/misc/WaterFX/WaterShaderHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Bootcamp.Misc.WaterFX
+{
+    [AddComponentMenu("Bootcamp/Misc/Water FX/Water Shader Helper")]
+    public class WaterShaderHelper : MonoBehaviour
+    {
+        [SerializeField] Transform _lightDirection;
+        private Material _material;
+
+        private void Start()
+        {
+            _material = GetComponent<SkinnedMeshRenderer>().material;
+        }
+
+        public void Update()
+        {
+            _material.shader.maximumLOD = QualitySettings.GetQualityLevel() * 100;
+
+            if (_lightDirection)
+                _material.SetVector("_WorldLightDir", _lightDirection.forward);
+            else
+                _material.SetVector("_WorldLightDir", new Vector3(0.7f, 0.7f, 0.0f));
+        }
+    }
+}
