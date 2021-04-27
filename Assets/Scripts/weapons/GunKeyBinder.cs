6f1006ec25a98d5b92f4785fe92e916fc7c9f86b
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-04-27 21:40:54 +0300

    GunKeyBinder to C#

---
+++ Assets/Scripts/weapons/GunKeyBinder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bootcamp.Weapons
+{
+    [System.Serializable]
+    public struct GunKeyBinder
+    {
+        public Gun gun;
+        public KeyCode keyToActivate;
+        public bool switchModesOnKey;
+    }
+}
