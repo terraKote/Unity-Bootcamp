c2d978275f4226d61853a2cc06a73c04016718f3
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-04-29 23:51:40 +0300

    AmbientParticleSettings to C#

---
+++ Assets/Scripts/huds/AmbientParticleSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class AmbientParticleSettings
+{
+    public float minSize;
+    public float maxSize;
+    public float minEmission;
+    public float maxEmission;
+};
