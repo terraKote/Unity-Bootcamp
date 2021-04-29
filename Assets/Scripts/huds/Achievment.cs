e07b0f4a5765553258a0396d0b5b9e5851434c56
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-04-30 00:08:45 +0300

    Achievment to C#

---
+++ Assets/Scripts/huds/Achievment.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class Achievment
+{
+    public string name;
+    public bool enabled;
+    public bool done;
+    public Texture2D icon;
+    public string description;
+    public int progress;
+    public int maxProgress;
+    public bool showProgress;
+}
