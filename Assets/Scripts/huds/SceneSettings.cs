0ba131e576639766aa502ffa0302ea907d020256
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-04-29 23:28:17 +0300

    SceneSettings to C#

---
+++ Assets/Scripts/huds/SceneSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSettings  {
+
+	//SCENE SETTINGS
+	public bool sceneInitialized = false;
+	
+	public float detailObjectDistance ;
+	public float detailObjectDensity ;
+	public float treeDistance ;
+	public float nearTerrainPixelError ;
+	public float terrainTreesBillboardStart ;
+	public int maxMeshTrees ;
+	public int heightmapMaximumLOD ;
+}
