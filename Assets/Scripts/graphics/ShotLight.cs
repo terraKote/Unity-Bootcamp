2c36d6e828e289bfbc8abdf059230b276eaeb4b2
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-04-29 22:13:35 +0300

    ShotLight to C#

---
+++ Assets/Scripts/graphics/ShotLight.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotLight : MonoBehaviour {
+
+	public float time = 0.02f;
+	private float timer;
+	
+	void OnEnable()
+	{
+		if (light == null)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			timer = time;
+			light.enabled = false;
+		}
+	}
+
+	void OnDisable()
+	{
+		if (light == null)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			timer = time;
+			light.enabled = false;
+		}
+	}
+
+	void LateUpdate()
+	{
+		timer -= Time.deltaTime;
+
+		if (timer <= 0.0)
+		{
+			timer = time;
+			light.enabled = !light.enabled;
+		}
+	}
+}
