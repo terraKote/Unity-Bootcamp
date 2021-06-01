9de77c5f9959155801117bed59b32ee19061d0af
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-06-01 22:13:40 +0300

    AutoDestroy to C#

---
+++ Assets/Scripts/misc/csharp/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public float time;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time < 0.0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
