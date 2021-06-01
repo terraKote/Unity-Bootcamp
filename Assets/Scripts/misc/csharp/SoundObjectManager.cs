d7b56a7dbf462fb60c2e924d45c53f90b7da1e17
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-06-01 22:22:36 +0300

    SoundObjectManager to C#

---
+++ Assets/Scripts/misc/csharp/SoundObjectManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SoundObjectManager : MonoBehaviour
+{
+    public float minSpeedToParticle = 3.0f;
+    public GameObject genericParticle;
+    public GameObject waterParticles;
+    public LayerMask waterLayer;
+
+    public float minSpeedSound = 2.0f;
+    public float maxSpeedSound = 10.0f;
+
+    public AudioClip defaultSound;
+    public AudioClip defaultMetalSound;
+    public AudioClip defaultWoodSound;
+    public AudioClip defaultConcreteSound;
+
+    public AudioClip[] additionalSounds;
+
+    static public SoundObjectManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
