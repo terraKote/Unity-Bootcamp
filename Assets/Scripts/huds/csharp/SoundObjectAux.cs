e52eaf833dfe0c30ea483598844b993b685d0fd7
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-06-01 23:43:22 +0300

    SoundObjectAux to C#

---
+++ Assets/Scripts/huds/csharp/SoundObjectAux.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundObjectAux : MonoBehaviour
+{
+    public SoundObject soundGenerator;
+
+    void Awake()
+    {
+        if (rigidbody != null) rigidbody.Sleep();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (soundGenerator == null)
+            Destroy(this);
+        else
+            soundGenerator.OnCollisionEnter(collision);
+    }
+}
