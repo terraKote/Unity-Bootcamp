70175ae908ba82191102308a3878346a43e06585
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-06-17 22:03:51 +0300

    MonoSingleton implementation

---
+++ Assets/Scripts/misc/csharp/MonoSingleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    private void Awake()
+    {
+        if (_instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this as T;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static T GetInstance()
+    {
+        return _instance;
+    }
+}
