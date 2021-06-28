894fc09a3df096aad807060fc14bd33878a467e2
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-06-28 20:15:26 +0300

    PausableBehaviour refactoring

--- Assets/Scripts/misc/csharp/PausableBehaviour.cs
+++ Assets/Scripts/misc/csharp/PausableBehaviour.cs
@@ -5,7 +5,7 @@ public abstract class PausableBehaviour : MonoBehaviour, IPauseListener
     private bool _isPaused;
     public bool IsPaused { get { return _isPaused; } }
 
-    public void OnSwitchPauseState(bool paused)
+    public virtual void OnSwitchPauseState(bool paused)
     {
         _isPaused = paused;
     }
