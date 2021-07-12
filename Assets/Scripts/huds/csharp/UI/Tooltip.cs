e2f33c4fdf886c51c12db808061931d4d3a52385
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-07-12 23:23:10 +0300

    Added: tooltip UI element

---
+++ Assets/Scripts/huds/csharp/UI/Tooltip.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasRenderer))]
+[RequireComponent(typeof(Image))]
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, Multiline] private string caption;
+    [SerializeField] private Sprite regularImage;
+    [SerializeField] private Sprite hoverImage;
+    [SerializeField] private TooltipPopup tooltipPopup;
+
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _image.sprite = hoverImage;
+
+        tooltipPopup.Caption = caption;
+        tooltipPopup.Position = transform.position;
+        tooltipPopup.Display(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _image.sprite = regularImage;
+        tooltipPopup.Display(false);
+    }
+}
---
+++ Assets/Scripts/huds/csharp/UI/TooltipPopup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TooltipPopup : MonoBehaviour
+{
+    [SerializeField] private Text label;
+    [SerializeField] private Vector2 offset = new Vector2(45, 0);
+
+    private Image _image;
+
+    public string Caption
+    {
+        get { return label.text; }
+        set { label.text = value; }
+    }
+
+    public Vector3 Position
+    {
+        get { return transform.position; }
+        set { transform.position = value + (Vector3)offset; }
+    }
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+
+        Display(false);
+    }
+
+    public void Display(bool active)
+    {
+        _image.enabled = active;
+        label.enabled = active;
+    }
+}
