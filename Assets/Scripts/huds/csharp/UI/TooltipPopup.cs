5fb636452e7ac561173ccf1e4f1634c009567f08
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2021-07-13 00:11:15 +0300

    Updated: tooltip fade in \ fade out

--- Assets/Scripts/huds/csharp/UI/TooltipPopup.cs
+++ Assets/Scripts/huds/csharp/UI/TooltipPopup.cs
@@ -1,19 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 [RequireComponent(typeof(CanvasGroup))]
+[ExecuteInEditMode]
 public class TooltipPopup : MonoBehaviour
 {
     [SerializeField] private Text label;
     [SerializeField] private Vector2 offset = new Vector2(45, 0);
+    [SerializeField] private int characterWrapLimit = 60;
+    [SerializeField] private float fadeSpeed = 0.01f;
 
     private CanvasGroup _canvasGroup;
+    private LayoutElement _layoutElement;
 
     public string Caption
     {
         get { return label.text; }
-        set { label.text = value; }
+        set { label.text = value; UpdateLayout(); }
     }
 
     public Vector3 Position
@@ -25,12 +31,55 @@ public class TooltipPopup : MonoBehaviour
     private void Awake()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
+        _layoutElement = GetComponent<LayoutElement>();
 
-        Display(false);
+        if (Application.isPlaying)
+        {
+            Display(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying) return;
+
+        UpdateLayout();
+    }
+
+    private void UpdateLayout()
+    {
+        _layoutElement.enabled = label.text.Length > characterWrapLimit ? true : false;
     }
 
     public void Display(bool active)
     {
-        _canvasGroup.alpha = active ? 1.0f : 0.0f;
+        StopAllCoroutines();
+        if (active)
+        {
+            _canvasGroup.alpha = 0.0f;
+            StartCoroutine(FadeIn());
+        }
+        else
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        while (_canvasGroup.alpha < 1.0f)
+        {
+            _canvasGroup.alpha += fadeSpeed;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        while (_canvasGroup.alpha > 0.0f)
+        {
+            _canvasGroup.alpha -= fadeSpeed;
+            yield return null;
+        }
     }
 }
